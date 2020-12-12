30d0cd822ccd15dd4b13a7e036c272f97c86dfae
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2020-12-12 11:17:46 +0100

    create

---
+++ t1.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Congratulations! Your first script executed successfully"
+while($true)
+{
+    $key = $Host.UI.RawUI.ReadKey()
+    if ($key.Character -eq [char]9) {
+    "Pressed tab"
+    }
+}
+
+
+write-host "Test-connection stopped jump to next command"
