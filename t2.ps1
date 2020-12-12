9d518f87a25fe9d6d01ac0c9a760341b2fcfef6b
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2020-12-12 11:27:25 +0100

    t2

---
+++ t2.ps1
@@ -0,0 +1,17 @@
+
+$key = '0x11' ## Ctrl
+
+$Signature = @'
+    [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+    public static extern short GetAsyncKeyState(int virtualKeyCode); 
+'@
+Add-Type -MemberDefinition $Signature -Name Keyboard -Namespace PsOneApi
+do
+{   If( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState($key) -eq -32767 ))
+        { 
+            Write-Host "You pressed the combo keys, oh my!!" -ForegroundColor Green 
+        }
+    
+      Start-Sleep -Milliseconds 100
+
+} while($true)
