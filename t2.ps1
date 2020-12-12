5f7618194f5f203c4eaa22c7dcda4870ecb5d7b2
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2020-12-12 14:30:01 +0100

    up

--- t2.ps1
+++ t2.ps1
@@ -1,5 +1,5 @@
 
-$key = '0x11' ## Ctrl
+$key = '0x10' ## Ctrl
 
 $Signature = @'
     [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
@@ -10,6 +10,12 @@ do
 {   If( [bool]([PsOneApi.Keyboard]::GetAsyncKeyState($key) -eq -32767 ))
         { 
             Write-Host "You pressed the combo keys, oh my!!" -ForegroundColor Green 
+            $wshell = New-Object -ComObject wscript.shell;
+            $wshell.AppActivate('title of the application window')
+
+            Sleep 0.1
+            wshell.SendKeys('ddd')
+
         }
     
       Start-Sleep -Milliseconds 100
